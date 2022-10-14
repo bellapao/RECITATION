f5df7ede2e8e8cdd8c8adc39360eeba63084b12d
bellapao <ispa5594@colorado.edu>
2022-10-13 20:03:30 -0600

    Update main.js

--- assignments/assignment8/main.js
+++ assignments/assignment8/main.js
@@ -12,7 +12,7 @@ function randomValueFromArray(array){
 storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
 const insertX = [
-    "Billy Bob",
+    "Billy Bo",
     "Joe Cool", 
     "Charlie Brown"]
 
