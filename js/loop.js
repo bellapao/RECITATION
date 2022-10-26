b8a9ad82d8d78687a6f6148be28f30ac8f25231b
bellapao <ispa5594@colorado.edu>
2022-10-26 17:24:23 -0600

    k

---
+++ js/loop.js
@@ -0,0 +1,25 @@
+const contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
+const para = document.querySelector('p');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  const searchName = input.value.toLowerCase();
+  input.value = '';
+  input.focus();
+  para.textContent = '';
+  for (const contact of contacts) {
+    const splitContact = contact.split(':');
+    //split the contacts at the ':' and store the resulting two values in an array called splitContact
+    if (splitContact[0].toLowerCase() === searchName) {
+      para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+      break;
+    }
+  }
+  if (para.textContent === '') {
+   para.textContent = 'Contact not found.';
+ }
+});
+
+//break : leave the loop 
+//contiune : skip to the next iteration of the loop
