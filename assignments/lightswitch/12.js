edf89fc406963591d2f5ce63dde584d6bfae9a78
bellapao <ispa5594@colorado.edu>
2022-12-09 14:10:37 -0700

    i

--- assignments/lightswitch/12.js
+++ assignments/lightswitch/12.js
@@ -1,6 +1,5 @@
 const light = document.getElementById('bulb');
 
-
 const discoRotate = [
   {transform: "rotateY(90deg)", easing:'ease-in'},
   {transform: "rotateY(0deg)"},
@@ -15,12 +14,6 @@ const discoTiming = {
   fill: "forwards",
 };
 
-const discoball = document.querySelector("#discoTest");
-light.addEventListener('click', () => {
-  var discotimeline = discoball.animate(discoRotate, discoTiming,);
-});
-
-
 const discRotate = [
   {transform: "rotateZ(360deg)" },
   {transform: "rotateZ(0deg)"},
@@ -30,35 +23,41 @@ const discRotate2 = [
   {transform: "rotateZ(0deg)"},
 ]
 
+const discoball = document.querySelector("#discoTest");
 const disc = document.querySelector("#disc");
 const disc2 = document.querySelector("#disc2");
 
 light.addEventListener('click', () => {
+  var discotimeline = discoball.animate(discoRotate, discoTiming,);
   disc.animate(discRotate, discoTiming,);
   disc2.animate(discRotate2,discoTiming);
   //document.getElementById("disc").style.animationName = 'move-right';
-});
-
-
-light.addEventListener('click',()=> {
   var background = 
     document.querySelector("body").animate({
     backgroundColor: ["red", "yellow", "green","pink","turquoise","purple"],},
     discoTiming,
     );
-})
+});
 
 
-light.addEventListener('click',function(e){
+let toggle = light.addEventListener('click',function(e){
    
     e.target.classList.toggle('bulb-on');
-    if (e.target.classList.includes('bulb-on')){
-    discotimeline.stop()
-    }
+    //if (e.target.classList.includes('bulb-on')){
+    //discotimeline.stop()}
 })
-
-//disco
-
-
+/*
+let lightstate = False;
+function lightToggle(toggle){
+  if (lightState == False){
+    //do all the animations
+    lightState = True;
+  }
+  else {
+    // kill all the animations
+    lightState = False;
+  }
+}
+*/
 
 
