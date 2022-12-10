9d702cd1423e27c37975ad9c3bcc1d84fda5f4e3
bellapao <ispa5594@colorado.edu>
2022-12-09 18:07:41 -0700

    ook

--- assignments/lightswitch/12.js
+++ assignments/lightswitch/12.js
@@ -1,16 +1,16 @@
 const light = document.getElementById('bulb');
 
 const discoRotate = [
-  {transform: "rotateY(90deg)", easing:'ease-in'},
-  {transform: "rotateY(0deg)"},
+  {transform: "rotateY(0deg)",easing:"ease-in"},
   {transform: "rotateY(90deg)"},
   {transform: "rotateY(0deg)"},
-  {transform: "rotateY(90deg)", easing: 'ease-out'},
+  {transform: "rotateY(90deg)"},
+  {transform: "rotateY(0deg)",easing:"ease-out"},
 ]
 
 const discoTiming = {
   duration: 3000,
-  iterations: Infinity,
+  iterations: 98,
   fill: "forwards",
 };
 
@@ -27,37 +27,51 @@ const discoball = document.querySelector("#discoTest");
 const disc = document.querySelector("#disc");
 const disc2 = document.querySelector("#disc2");
 
+
 light.addEventListener('click', () => {
   var discotimeline = discoball.animate(discoRotate, discoTiming,);
-  disc.animate(discRotate, discoTiming,);
-  disc2.animate(discRotate2,discoTiming);
-  //document.getElementById("disc").style.animationName = 'move-right';
+  var disctimeline = disc.animate(discRotate, discoTiming,);
+  var disc2timeline = disc2.animate(discRotate2,discoTiming);
   var background = 
     document.querySelector("body").animate({
-    backgroundColor: ["red", "yellow", "green","pink","turquoise","purple"],},
+    backgroundColor: ["red", "yellow", "green","pink","turquoise","purple","#f0ffff"],},
     discoTiming,
     );
+  var audio = new Audio ("BoogieWonderland.mp3"); audio.play()
 });
 
 
 let toggle = light.addEventListener('click',function(e){
-   
-    e.target.classList.toggle('bulb-on');
+   e.target.classList.toggle('bulb-on');
     //if (e.target.classList.includes('bulb-on')){
-    //discotimeline.stop()}
-})
+    //discotimeline.pause()}
+});
+
 /*
 let lightstate = False;
-function lightToggle(toggle){
+function lightToggle(){
   if (lightState == False){
     //do all the animations
-    lightState = True;
+      var discotimeline = discoball.animate(discoRotate, discoTiming,);
+      var disctimeline = disc.animate(discRotate, discoTiming,);
+      var disc2timeline = disc2.animate(discRotate2,discoTiming);
+      var background = 
+        document.querySelector("body").animate({
+        backgroundColor: ["red", "yellow", "green","pink","turquoise","purple"],},
+        discoTiming,);
+      var audio = new Audio ("BoogieWonderland.mp3"); audio.play()
+  lightState = True;
   }
   else {
     // kill all the animations
+    // animate.pause
+        audio.pause;
+        discotimeline.pause;
+        disctimeline.pause;
+        disc2timeline.pause;
     lightState = False;
   }
 }
+
+discoball.addEventListener("click", audio.pause());
 */
-
-
